2a89033f6470c8cef48a5654fdd78b86bc02b4a7
Gaurav chauhan <gauravchauhan2403@gmail.com>
2021-07-09 14:11:44 +0530

    quiz app

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by gaurav.
+    </div>
+  );
+};
+
+export default Footer;
